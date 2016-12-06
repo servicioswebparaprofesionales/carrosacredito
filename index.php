e06e3a52bd45b80ed13714d059127042c6428028
MJCR <mjcrsis22@Gmail.com>
2016-12-06 16:05:43 -0400

    frst commit

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+<title>City Cars de Venezuela</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="assets/css/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://use.fontawesome.com/e1a5a1ba4d.js"></script>
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+.w3-navbar,h1,button {font-family: "Montserrat", sans-serif}
+.fa-anchor,.fa-coffee {font-size:200px}
+</style>
+<!-- Navbar -->
+<div class="w3-top">
+  <ul class="w3-navbar w3-blue w3-card-2 w3-left-align w3-large">
+    <li class="w3-hide-medium w3-hide-large w3-opennav w3-right">
+      <a class="w3-padding-large w3-hover-white w3-large w3-blue" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    </li>
+    <li>
+      <a href="#" class="w3-padding-large w3-white" title="City Cars de Venezuela">
+        <i class="fa fa-car w3-text-blue" style="margin-right: .5em;"></i>City Cars
+      </a>
+    </li>
+    <li class="w3-hide-small"><a href="#" class="w3-padding-large w3-hover-white">Nosotros</a></li>
+    <li class="w3-hide-small"><a href="#" class="w3-padding-large w3-hover-white">Planes de financiamiento</a></li>
+    <li class="w3-hide-small"><a href="#" class="w3-padding-large w3-hover-white">Contactanos</a></li>
+    <li class="w3-hide-small"><a href="#" class="w3-padding-large w3-hover-white">Planilla de contrato</a></li>
+  </ul>
+
+  <!-- Navbar on small screens -->
+  <div id="navDemo" class="w3-hide w3-hide-large w3-hide-medium">
+    <ul class="w3-navbar w3-left-align w3-large w3-blue">
+      <li><a class="w3-padding-large w3-hover-white" href="#">Nosotros</a></li>
+      <li><a class="w3-padding-large w3-hover-white" href="#">Planes de financiamiento</a></li>
+      <li><a class="w3-padding-large w3-hover-white" href="#">Contactanos</a></li>
+      <li><a class="w3-padding-large w3-hover-white" href="#">Planilla de contrato</a></li>
+    </ul>
+  </div>
+</div>
+
+<!-- Header -->
+<header class="w3-container w3-white w3-center w3-padding-128">
+  <h1 class="w3-margin w3-jumbo w3-text-blue">EMPIEZA HOY</h1>
+  <p class="w3-xlarge w3-text-blue">Llena tu planilla de contrato y nos pondremos en contacto</p>
+  <button class="w3-btn w3-padding-16 w3-large w3-margin-top w3-blue">Llenar planilla</button>
+  <br><i class="fa fa-car w3-text-indigo w3-padding-64" style="font-size: 8em;"></i>
+</header>
+
+<!-- First Grid -->
+<div class="w3-row-padding w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-twothird">
+      <h1>Introductorio de la P&aacute;gina</h1>
+      <h5 class="w3-padding-12">Mensaje explicativo sobre el contenido que se le presentar&aacute; a continuaci&oacute;n, a trav&eacute;s de las diferentes p&aacute;ginas del tiene este contenido ira cambiando</h5>
+
+      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+
+    <div class="w3-third w3-center">
+      <i class="fa fa-file-text w3-text-blue w3-padding-32" style="font-size: 16em;"></i>
+    </div>
+  </div>
+</div>
+
+<!-- Second Grid -->
+<div class="w3-row-padding w3-light-grey w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-third w3-center">
+      <i class="fa fa-rocket w3-text-blue w3-padding-32 w3-margin-right" style="font-size: 16em;"></i>
+    </div>
+
+    <div class="w3-twothird">
+      <h1>Contenido del la pa&aacute;gina</h1>
+      <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
+
+      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+  </div>
+</div>
+
+<div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
+    <h1 class="w3-margin w3-xlarge">Un viaje de mil millas comienza con el primer paso.</h1>
+    <h3>Lao-ts&eacute;.</h3>
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-opacity">
+   <div class="w3-row">
+  <div class="w3-col m8 l8">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d655.1012953512959!2d-67.58881420931864!3d10.247742497065229!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xff73b24c72c20b28!2sGlobal+Autopartes+%26+Suministros+C.A.!5e0!3m2!1ses!2sve!4v1481050238594" frameborder="0" style="border:0;width:90%;"></iframe>
+  </div>
+  <div class="w3-col m4 l4">
+     <h2>Visitanos:</h2>
+     <p>Maracay, edo. Aragua, av. Miranda a una cuadra de la av. Fuerzas A&eacute;reas, C.C.Clavellinas, edf. Fripollo, piso 1, local 7, frente a la Toyota (Toyocenter).</p>
+  </div>
+</div>
+</footer>
+
+<script>
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+</script>
+
+</body>
+</html>
